1d9b8e585d2c956ea4968772807e5b96d21bddb1
Lucas Olivera <oliveralucasm@gmail.com>
2021-04-02 16:52:58 -0300

    Refator time constants

--- app/src/App.js
+++ app/src/App.js
@@ -15,6 +15,7 @@ import TestEntry from './components/TestEntry'
 
 function App() {
   const entry = new TestEntry()
+  const baseTime = 30000
   
   return (
     <Router>
@@ -23,9 +24,9 @@ function App() {
         <Route path='/text1' exact component={() => <TextView testPhase={1} testEntry={entry}/>} />
         <Route path='/text2' exact component={() => <TextView testPhase={2} testEntry={entry}/>} />
         <Route path='/text3' exact component={() => <TextView testPhase={3} testEntry={entry}/>} />
-        <Route path='/test0' exact component={() => <TestView images={getImages(0)} time={300000} testPhase={0} testEntry={entry} />} />
-        <Route path='/test1' exact component={() => <TestView images={getImages(1)} time={600000} testPhase={1} testEntry={entry} />} />
-        <Route path='/test2' exact component={() => <DrawingView images={getImages(2)} time={60000} testPhase={2} testEntry={entry} />} />
+        <Route path='/test0' exact component={() => <TestView images={getImages(0)} time={baseTime} testPhase={0} testEntry={entry} />} />
+        <Route path='/test1' exact component={() => <TestView images={getImages(1)} time={baseTime*2} testPhase={1} testEntry={entry} />} />
+        <Route path='/test2' exact component={() => <DrawingView images={getImages(2)} time={baseTime*0.2} testPhase={2} testEntry={entry} />} />
         <Route path='/report' exact component={() => <EntriesReportView />} />
         <Route path='/' exact component={() => <StartView testEntry={entry} />} />
       </Switch>
